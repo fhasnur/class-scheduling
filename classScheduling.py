0e3de635f08870c3b491b2ae5b3d633c4a3a25a2
Fandi Hasnur <fandi.hasnur10@gmail.com>
2021-08-19 13:52:29 +0800

    fix errors and add method in genetic algorithm
    fix errors and add method in genetic algorithm class

--- classScheduling.py
+++ classScheduling.py
@@ -2,6 +2,9 @@ import prettytable as prettytable
 import random as rnd
 
 POPULATION_SIZE = 9
+NUMB_OF_ELITE_SCHEDULES = 1
+TOURNAMENT_SELECTION_SIZE = 3
+MUTATION_RATE = 0.1
 
 
 class Data:
@@ -160,7 +163,54 @@ class Population:
 
 
 class GeneticAlgorithm:
-    """"""
+    def evolve(self, population):
+        return self._mutate_population(self._crossover_population(population))
+
+    def _crossover_population(self, pop):
+        crossover_pop = Population(0)
+        for i in range(NUMB_OF_ELITE_SCHEDULES):
+            crossover_pop.get_schedules().append(pop.get_schedules()[i])
+        i = NUMB_OF_ELITE_SCHEDULES
+        while i < POPULATION_SIZE:
+            schedule1 = self._select_tournament_population(pop).get_schedules()[0]
+            schedule2 = self._select_tournament_population(pop).get_schedules()[0]
+            crossover_pop.get_schedules().append(
+                self._crossover_schedule(schedule1, schedule2)
+            )
+            i += 1
+        return crossover_pop
+
+    def _mutate_population(self, population):
+        for i in range(NUMB_OF_ELITE_SCHEDULES, POPULATION_SIZE):
+            self._mutate_schedule(population.get_schedules()[i])
+        return population
+
+    def _crossover_schedule(self, schedule1, schedule2):
+        crossoverSchedule = Schedule().initialize()
+        for i in range(0, len(crossoverSchedule.get_classes())):
+            if rnd.random() > 0.5:
+                crossoverSchedule.get_classes()[i] = schedule1.get_classes()[i]
+            else:
+                crossoverSchedule.get_classes()[i] = schedule2.get_classes()[i]
+        return crossoverSchedule
+
+    def _mutate_schedule(self, mutateSchedule):
+        schedule = Schedule().initialize()
+        for i in range(0, len(mutateSchedule.get_classes())):
+            if MUTATION_RATE > rnd.random():
+                mutateSchedule.get_classes()[i] = schedule.get_classes()[i]
+        return mutateSchedule
+
+    def _select_tournament_population(self, pop):
+        tournament_pop = Population(0)
+        i = 0
+        while i < TOURNAMENT_SELECTION_SIZE:
+            tournament_pop.get_schedules().append(
+                pop.get_schedules()[rnd.randrange(0, POPULATION_SIZE)]
+            )
+            i += 1
+        tournament_pop.get_schedules().sort(key=lambda x: x.get_fitness(), reverse=True)
+        return tournament_pop
 
 
 class Course:
@@ -354,7 +404,7 @@ class DisplayMgr:
         meetingTimes = data.get_meetingTimes()
         for i in range(0, len(meetingTimes)):
             availableMeetingTimeTable.add_row(
-                [meetingTimes[i].get_id(), meetingTimes[i].get_time]
+                [meetingTimes[i].get_id(), meetingTimes[i].get_time()]
             )
         print(availableMeetingTimeTable)
 
@@ -364,7 +414,7 @@ class DisplayMgr:
                 "schedules #",
                 "fitness",
                 "# of conflicts",
-                "classes [dept,class, room, instructos",
+                "classes [dept,class, room, instructor, meeting-time]",
             ]
         )
         schedules = population.get_schedules()
